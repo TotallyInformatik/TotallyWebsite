976cbe03d5830f572a1e000098007ab2486e322d
Rui Zhang <totallyturing@gmail.com>
2022-04-13 00:17:02 +0200

    firebase api test 2, now with environment variables

--- pages/api/firebase.ts
+++ pages/api/firebase.ts
@@ -9,26 +9,24 @@ type Data = {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
 
-  const firebaseConfig = {
-    apiKey: process.env.SECRET_API_KEY,
-    authDomain: process.env.SECRET_AUTH_DOMAIN,
-    projectId: process.env.SECRET_PROJECT_ID,
-    storageBucket: process.env.SECRET_STORAGE_BUCKET,
-    messagingSenderId: process.env.SECRET_MESSAGING_SENDER_ID,
-    appId: process.env.SECRET_APP_ID,
-    measurementId: process.env.SECRET_MEASUREMENT_ID
-  };
-
-  // Initialize Firebase
-  //const app: FirebaseApp = initializeApp(firebaseConfig);
-  //const db: Firestore = getFirestore(app);
-
-  //const firebaseResult: DocumentSnapshot = await getDoc(doc(db, "links/publicLinks"));
-
   if (req.method === "GET") {
+    const firebaseConfig = {
+      apiKey: process.env.SECRET_API_KEY,
+      authDomain: process.env.SECRET_AUTH_DOMAIN,
+      projectId: process.env.SECRET_PROJECT_ID,
+      storageBucket: process.env.SECRET_STORAGE_BUCKET,
+      messagingSenderId: process.env.SECRET_MESSAGING_SENDER_ID,
+      appId: process.env.SECRET_APP_ID,
+      measurementId: process.env.SECRET_MEASUREMENT_ID
+    };
+  
+    // Initialize Firebase
+    const app: FirebaseApp = initializeApp(firebaseConfig);
+    const db: Firestore = getFirestore(app);
+  
+    const firebaseResult: DocumentSnapshot = await getDoc(doc(db, "links/publicLinks"));
 
-    res.status(200).json({message: process.env.SECRET_API_KEY});
-
+    res.status(200).json({message: firebaseResult.data.toString()});
   }
 
 }
