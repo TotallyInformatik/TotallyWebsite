f2afcc07296189322bafa196fa4dc84a46ddf657
Rui Zhang <totallyturing@gmail.com>
2022-06-30 14:53:31 +0200

    added google verification

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,8 @@ export default function Document() {
       <Head>
         <link rel="shortcut icon" href="/favicon.ico"></link>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        
+        <meta name="google-site-verification" content="GIcGUItqREvdYAq5D7uqywR4-_NrpVPl1Nr2AYWbJTk" />
       </Head>
       <body>
         <Main />
